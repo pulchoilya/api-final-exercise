07773cae94da9ba5ea023e22217f92eeba737152
Ilya Pulcho <ilyapulcho@gmail.com>
2026-08-22 16:48:24 +0300

    refactor: remove unused seedUser export

--- tests/app/testData/seedUsers.ts
+++ tests/app/testData/seedUsers.ts
@@ -2,8 +2,3 @@ export const seedAdmin = {
   email: process.env.SEED_ADMIN_EMAIL ?? 'admin@dojo.api',
   password: process.env.SEED_ADMIN_PASSWORD ?? 'Password1',
 };
-
-export const seedUser = {
-  email: process.env.SEED_USER_EMAIL ?? 'user@dojo.api',
-  password: process.env.SEED_USER_PASSWORD ?? 'Password1',
-};
